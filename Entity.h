3e03611dfec2075917038ccc87b2c1e396cb9727
tyyl2912-wq <tyyl2912@gmail.com>
2026-08-18 22:34:53 +0800

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -1,13 +1,20 @@
+#include<string>
 #pragma once
 class Entity{
 private:
 	double damage;
+	//health is the entitys current health, max_health is the maximum health the entity can have
 	double health;
 	double max_health;
+	std::string name;
 public:
-	Entity(double d, double h, double mh);
+	Entity(double d, double h, double mh, std::string n);
 	
+	//call this function to let enemy take damage
+	void takeDamage(double damageTaken);
+
 	double getDamage();
+	//used to apply buffs/debuffs
 	double changeDamage(double change);
 	
 	double getHealth();
