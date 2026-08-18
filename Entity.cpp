3b5760d4dc433043a3d383bec518db986a53ad3f
tyyl2912-wq <tyyl2912@gmail.com>
2026-08-18 21:37:35 +0800

    Implement health and damage management in Entity class
    Refactored Entity class to include health and damage attributes with corresponding methods.

--- Entity.cpp
+++ Entity.cpp
@@ -1,12 +1,39 @@
 #include "Entity.h"
 
-Entity::Entity()
-{
-	int health;
-	int maxhealth;
+Entity::Entity(double d, double h, double mh){
+	damage = d;
+	health = h;
+	max_health = mh;
 }
 
-void::Entity()
+double Entity::getDamage()
 {
-	
+	return damage;
 }
+
+double Entity::changeDamage(double change)
+{
+	return damage + change;
+}
+
+ double Entity::getHealth()
+{
+	return health;
+}
+
+ double Entity::changeHealth(double change)
+ {
+	 return health + change;
+ }
+
+ double Entity::getMaxHealth()
+ {
+	 return max_health;
+ }
+
+ double Entity::changeMaxHealth(double change)
+ {
+	 return max_health + change;
+ }
+
+
