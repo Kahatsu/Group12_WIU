c3a935ada7d8971449394cf035ca675b6961e49b
va727 <tohwenxuan3@gmail.com>
2026-08-18 10:09:38 +0800

    Add files via upload

---
+++ enemy.cpp
@@ -0,0 +1,21 @@
+#include "enemy.h"
+
+
+
+
+int enemy::getx() {
+	return x;
+}
+
+
+int enemy::gety() {
+	return y;
+}
+
+
+
+
+int enemy::getturns() {
+	return turns;
+}
+
---
+++ enemy.h
@@ -0,0 +1,29 @@
+#pragma once
+class enemy
+{
+
+	
+
+	int x;
+	int y;
+
+
+	int turns;
+
+public:
+	
+
+
+	int getx();
+	int gety();
+
+	
+
+	int getturns();
+
+
+	void dropitem();
+	void dropmoney(int m);
+
+};
+
