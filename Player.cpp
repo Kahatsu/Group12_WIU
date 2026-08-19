a88fd11f57aff433c49a70266b58dca186789e03
tyyl2912-wq <tyyl2912@gmail.com>
2026-08-19 15:36:36 +0800

    Refactor Player class with new attributes and methods

--- Player.cpp
+++ Player.cpp
@@ -1,37 +1,111 @@
 #include "Player.h"
 
-Player::Player()
+Player::Player():Entity(10,100,100,"placeholder")
 {
-    x = 0;
-    y = 0;
+    weight = 50;
+    money = 10;
+	x_coord = 2;
+	y_coord = 4;
 }
 
-void Player::moveUp()
+double Player::getWeight()
 {
-    y--;
+    return weight;
+}   
+
+double Player::reduceWeight(double minus)
+{
+    return weight - minus;
 }
 
-void Player::moveDown()
+double Player::gainWeight(double add)
 {
-    y++;
+    return weight + add;
 }
 
-void Player::moveLeft()
+int Player::getMoney()
 {
-    x--;
+    return money;
 }
 
-void Player::moveRight()
+void Player::gainMoney(int value)
 {
-    x++;
+    money += value;
 }
 
-int Player::getX() const
+void Player::loseMoney(int value)
 {
-    return x;
+    money -= value;
 }
 
-int Player::getY() const
+void Player::equip()
 {
-    return y;
+
+}
+
+void Player::openInventory()
+{
+    inventory.displayInventoryUI();
+}
+
+void Player::pickUpItem()
+{
+    
+}
+
+void Player::move(char key, Map* map)
+{
+	switch (key) {
+	case 'w':
+		if (y_coord != 0 && (map->checkMap(x_coord, y_coord - 1) == 'o')) {
+			map->updateMap('o', x_coord, y_coord);
+			y_coord -= 1;
+			map->updateMap('P', x_coord, y_coord);
+		}
+		break;
+	case 's':
+		if (y_coord != 9 && (map->checkMap(x_coord, y_coord + 1) == 'o')) {
+			map->updateMap('o', x_coord, y_coord);
+			y_coord += 1;
+			map->updateMap('P', x_coord, y_coord);
+		}
+		break;
+	case 'a':
+		if (x_coord != 0 && (map->checkMap(x_coord - 1, y_coord) == 'o')) {
+			map->updateMap('o', x_coord, y_coord);
+			x_coord -= 1;
+			map->updateMap('P', x_coord, y_coord);
+		}
+		break;
+	case 'd':
+		if (x_coord != 9 && (map->checkMap(x_coord + 1, y_coord) == 'o')) {
+			map->updateMap('o', x_coord, y_coord);
+			x_coord += 1;
+			map->updateMap('P', x_coord, y_coord);
+		}
+		break;
+	default:
+		map->updateMap('P', x_coord, y_coord);
+		break;
+	}
+}
+
+int Player::getXcoord()
+{
+    return x_coord;
+}
+
+void Player::setXcoord(int new_pos)
+{
+    x_coord = new_pos;
+}
+
+int Player::getYcoord()
+{
+    return y_coord;
+}
+
+void Player::setYcoord(int new_pos)
+{
+    y_coord = new_pos;
 }
