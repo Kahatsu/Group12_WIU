043a764811d81b2db7f1d8190d77fd7c8b07e5a2
tyyl2912-wq <tyyl2912@gmail.com>
2026-08-19 15:39:10 +0800

    Add methods to check and update the map
    Added checkMap and updateMap methods to the Map class.

--- Map.h
+++ Map.h
@@ -9,4 +9,8 @@ public:
     Map();
     void loadMap(int floorNum);
     void drawMap();
+
+    char checkMap(int x_coord, int y_coord);
+
+    void updateMap(char symbol, int x_coord, int y_coord);
 };
