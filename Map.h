9239e9f96cbc169f4190423781c8497808d843d5
tyyl2912-wq <tyyl2912@gmail.com>
2026-08-20 19:56:15 +0800

    Expand Map class with player coordinates and display map

--- Map.h
+++ Map.h
@@ -3,8 +3,10 @@
 class Map
 {
 private:
-    char map[10][10];
-
+    char map[80][90];
+    int playerXCoord;
+    int playerYCoord;
+    char displayMap[11][11];
 public:
     Map();
     void loadMap(int floorNum);
@@ -13,4 +15,9 @@ public:
     char checkMap(int x_coord, int y_coord);
 
     void updateMap(char symbol, int x_coord, int y_coord);
+
+    void setPlayerXCoord(int x);
+    void setPlayerYCoord(int y);
+
+    void updateDisplayMap();
 };
