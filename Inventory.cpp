3dac19848ef5a99c60fc1fae879e54df5fa3b967
tyyl2912-wq <tyyl2912@gmail.com>
2026-08-20 20:00:08 +0800

    Update Inventory.cpp

--- Inventory.cpp
+++ Inventory.cpp
@@ -2,30 +2,57 @@
 #include<algorithm>
 #include<vector>
 #include<iostream>
+#include<array>
 #include "Inventory.h"
 
-Inventory::Inventory() {
-	weapons.reserve(8);
-	armors.reserve(8);
+Inventory::Inventory(){
+	weapons.reserve(11);
+	armors.reserve(3);
+
+	consumables_list = { "Potion","SmokeBomb" };
+	weapons_list = { "Gauntlets","Sword","Slingshot","Bow","Scythe","Dagger","Golden Vow Axe","Excalibur","Gate of Babylon","Luminosite Eternelle","Kanshou and Bakuya" };
+	armors_list = { "Iron Armor","Diamond Armor","Netherite Armor" };
 }
 
-void Inventory::addConsumable(std::string item, int quantity) {
-	consumables[item] += quantity;
+void Inventory::addItems(Items& item)
+{
+	//check if consumable
+	for (int i = 0;i < consumables_list.size(); i++) {
+		if (item.getName() == consumables_list[i]) {
+			addConsumable(item.getName());
+		}
+	}
+	//check if weapon
+	for (int i = 0;i < weapons_list.size(); i++) {
+		if (item.getName() == weapons_list[i]) {
+			addWeapon(item.getName());
+		}
+	}
+	//check if armor						   
+	for (int i = 0;i < armors_list.size(); i++) {
+		if (item.getName() == armors_list[i]) {
+			addArmor(item.getName());
+		}
+	}
 }
 
-void Inventory::removeConsumable(std::string item, int quantity) {
-	consumables[item] -= quantity;
+void Inventory::addConsumable(std::string item){
+	consumables[item]++;
+}
+
+void Inventory::removeConsumable(std::string item){
+	consumables[item]--;
 
 	if (consumables[item] <= 0) {
 		consumables.erase(item);
 	}
 }
 
-void Inventory::addWeapon(std::string weapon) {
+void Inventory::addWeapon(std::string weapon){
 	weapons.push_back(weapon);
 }
 
-void Inventory::dropWeapon(std::string weapon) {
+void Inventory::dropWeapon(std::string weapon){
 	auto it = std::find(weapons.begin(), weapons.end(), weapon);
 
 	if (it != weapons.end()) {
@@ -33,11 +60,11 @@ void Inventory::dropWeapon(std::string weapon) {
 	}
 }
 
-void Inventory::addArmor(std::string armor) {
+void Inventory::addArmor(std::string armor){
 	armors.push_back(armor);
 }
 
-void Inventory::dropArmor(std::string armor) {
+void Inventory::dropArmor(std::string armor){
 	auto it = std::find(armors.begin(), armors.end(), armor);
 
 	if (it != armors.end()) {
@@ -45,225 +72,86 @@ void Inventory::dropArmor(std::string armor) {
 	}
 }
 
-void Inventory::clearConsole() {
+void Inventory::clearConsole(){
 	std::cout << "\033[H\033[2J" << std::flush;
 }
 
-//void Inventory::displayInventoryUI() {
-//	std::cout << "1. Items  2. Weapons  3. Armors:  ";
-//	int choice{};
-//	std::cin >> choice;
-//
-//	clearConsole();
-//
-//	switch (choice) {
-//	case 1: {
-//		std::cout << "Items: \n";
-//		int index = 0;
-//		for (auto& item : consumables) {
-//			std::cout << index + 1 << " " << item.first << "  x" << item.second << "\n";
-//			index++;
-//		}
-//		std::cout << index + 1 << ". Escape\n";
-//		std::cout << "Option: ";
-//
-//		int option{};
-//		std::cin >> option;
-//		//break out early if option is invalid
-//		if (option < 1 || option > index) {
-//			break;
-//		}
-//
-//		int counter{};
-//		for (auto& item : consumables) {
-//			if (counter == option - 1) {
-//				std::cout << "You consumed " << item.first << "\n";
-//				removeConsumable(item.first, 1);
-//				break;
-//			}
-//			counter++;
-//		}
-//
-//		break;
-//	}
-//	case 2: {
-//		for (int i = 0; i < weapons.size(); i++) {
-//			std::cout << i + 1 << ". " << weapons[i] << "\n";
-//		}
-//		std::cout << weapons.size() + 1 << ". Escape\n";
-//		std::cout << "Option: ";
-//
-//		int option{};
-//		std::cin >> option;
-//		//break out early if option is invalid
-//		if (option < 1 || option > weapons.size()) {
-//			break;
-//		}
-//
-//		std::cout << "You have equipped " << weapons[option - 1] << "\n";
-//
-//		break;
-//	}
-//	case 3: {
-//		for (int i = 0; i < armors.size(); i++) {
-//			std::cout << i + 1 << ". " << armors[i] << "\n";
-//		}
-//		std::cout << armors.size() + 1 << ". Escape\n";
-//		std::cout << "Option: ";
-//
-//		int option{};
-//		std::cin >> option;
-//		//break out early if option is invalid
-//		if (option < 1 || option > armors.size()) {
-//			break;
-//		}
-//
-//		std::cout << "You have equipped " << armors[option - 1] << "\n";
-//
-//		break;
-//	}
-//	default:
-//		break;
-//	}
-//}
+void Inventory::displayInventoryUI(){
+	std::cout << "1. Items  2. Weapons  3. Armors:  ";
+	int choice{};
+	std::cin >> choice;
 
-void Inventory::displayInventoryUI()
-{
-    bool inventoryOpen = true;
+	clearConsole();
 
-    while (inventoryOpen)
-    {
-        clearConsole();
+	switch (choice) {
+	case 1: {
+		std::cout << "Items: \n";
+		int index = 0;
+		for (auto& item : consumables) {
+			std::cout << index + 1 << " " << item.first << "  x" << item.second << "\n";
+			index++;
+		}
+		std::cout << index + 1 << ". Escape\n";
+		std::cout  << "Option: ";
+		
+		int option{};
+		std::cin >> option;
+		//break out early if option is invalid
+		if (option < 1 || option > index) {
+			break;
+		}
 
-        std::cout << "1. Items  2. Weapons  3. Armors  4. Escape: ";
+		int counter{};
+		for (auto& item : consumables) {
+			if (counter == option - 1) {
+				std::cout << "You consumed " << item.first << "\n";
+				removeConsumable(item.first);
+				break;
+			}
+			counter++;
+		}
+		
+		break;
+	}
+	case 2: {
+		for (int i = 0; i < weapons.size(); i++) {
+			std::cout << i + 1 << ". " << weapons[i] << "\n";
+		}
+		std::cout << weapons.size() + 1 << ". Escape\n";
+		std::cout << "Option: ";
 
-        int choice{};
-        std::cin >> choice;
+		int option{};
+		std::cin >> option;
+		//break out early if option is invalid
+		if (option < 1 || option > weapons.size()) {
+			break;
+		}
 
-        clearConsole();
+		std::cout << "You have equipped " << weapons[option - 1] << "\n";
 
-        switch (choice)
-        {
-        case 1:
-        {
-            std::cout << "Items:\n";
+		break;
+	}
+	case 3: {
+		for (int i = 0; i < armors.size(); i++) {
+			std::cout << i + 1 << ". " << weapons[i] << "\n";
+		}
+		std::cout << armors.size() + 1 << ". Escape\n";
+		std::cout << "Option: ";
 
-            int index = 0;
+		int option{};
+		std::cin >> option;
+		//break out early if option is invalid
+		if (option < 1 || option > armors.size()) {
+			break;
+		}
 
-            for (auto& item : consumables)
-            {
-                std::cout << index + 1 << ". "
-                    << item.first
-                    << " x"
-                    << item.second
-                    << "\n";
+		std::cout << "You have equipped " << armors[option - 1] << "\n";
 
-                index++;
-            }
-
-            std::cout << index + 1 << ". Escape\n";
-            std::cout << "Option: ";
-
-            int option{};
-            std::cin >> option;
-
-            if (option >= 1 && option <= index)
-            {
-                int counter = 0;
-
-                for (auto& item : consumables)
-                {
-                    if (counter == option - 1)
-                    {
-                        std::cout << "You consumed "
-                            << item.first
-                            << "\n";
-
-                        removeConsumable(item.first, 1);
-                        break;
-                    }
-
-                    counter++;
-                }
-
-                std::cout << "\nPress Enter to continue...";
-                std::cin.ignore();
-                std::cin.get();
-            }
-
-            break;
-        }
-
-        case 2:
-        {
-            for (int i = 0; i < weapons.size(); i++)
-            {
-                std::cout << i + 1
-                    << ". "
-                    << weapons[i]
-                    << "\n";
-            }
-
-            std::cout << weapons.size() + 1
-                << ". Escape\n";
-
-            std::cout << "Option: ";
-
-            int option{};
-            std::cin >> option;
-
-            if (option >= 1 && option <= weapons.size())
-            {
-                std::cout << "You have equipped "
-                    << weapons[option - 1]
-                    << "\n";
-
-                std::cout << "\nPress Enter to continue...";
-                std::cin.ignore();
-                std::cin.get();
-            }
-
-            break;
-        }
-
-        case 3:
-        {
-            for (int i = 0; i < armors.size(); i++)
-            {
-                std::cout << i + 1
-                    << ". "
-                    << armors[i]
-                    << "\n";
-            }
-
-            std::cout << armors.size() + 1
-                << ". Escape\n";
-
-            std::cout << "Option: ";
-
-            int option{};
-            std::cin >> option;
-
-            if (option >= 1 && option <= armors.size())
-            {
-                std::cout << "You have equipped "
-                    << armors[option - 1]
-                    << "\n";
-
-                std::cout << "\nPress Enter to continue...";
-                std::cin.ignore();
-                std::cin.get();
-            }
-
-            break;
-        }
-
-        case 4:
-            inventoryOpen = false;
-            break;
-
-        default:
-            break;
-        }
-    }
+		break;
+	}
+	default:
+		break;
+	}
 }
+
+
