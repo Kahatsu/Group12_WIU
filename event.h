7d13ac40336cbd44ae0293a89582355a01cb3230
Komi <jervisneohb@gmail.com>
2026-08-21 09:54:35 +0800

    Update event.h

--- event.h
+++ event.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Player.h"
 class Event
 {
 public:
@@ -6,9 +7,21 @@ public:
 	int choice;
 
 
+//Event distributor
+	void triggerevent(Player& Player);
 
+//Function to call each specific event 
 
-
+	void event_Fountain(Player& Player);//1
+	void event_Shrine(Player& Player);//2
+	void event_WheelOfFate(Player& Player);//3
+	void event_AnimalLover(Player& Player);//4
+	void event_TarotMania(Player& Player);//5
+	void event_Goddess(Player& Player);//6
+	void event_DevilsDeal(Player& Player);//7
+	void event_Smithy(Player& Player);//8
+	void event_DatingSim(Player& Player);//9
+	void event_FourthWall(Player& Player);//10
 
 
 
