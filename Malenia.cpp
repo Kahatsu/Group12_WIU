0b5b82bfbdec2a33649c9a9eefb8f22c70b576c2
dumuncle07 <dumuncle07@gmail.com>
2026-08-24 09:49:10 +0800

    Enhance Malenia's attack methods with player damage
    Updated Malenia's attack and special_attack methods to include player interaction and lifesteal mechanics.

--- Malenia.cpp
+++ Malenia.cpp
@@ -1,20 +1,49 @@
 #include "Malenia.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 Malenia::Malenia()
-    : Enemy(40, 150, 150, "Malenia", 20)
+    : Enemy(40, 150, 150, "Malenia", 50)
 {
     lifestealPercent = 0.05;
+
+    // Seed random number generator
+    srand(static_cast<unsigned int>(time(0)));
 }
 
-void Malenia::attack()
+void Malenia::attack(Player* player)
 {
+    player->takeDamage(getDamage());
+
+    double healAmount = getMaxHealth() * lifestealPercent;
+    changeHealth(healAmount);
+
+    if (getHealth() > getMaxHealth())
+    {
+        changeHealth(getMaxHealth() - getHealth());
+    }
+
     std::cout << "Malenia attacks!" << std::endl;
+    std::cout << "Malenia regained "
+        << healAmount
+        << " HP!" << std::endl;
 }
 
-void Malenia::special_attack()
+void Malenia::special_attack(Player* player)
 {
-    std::cout << "Malenia uses her special attack!, Waterfowl Dance" << std::endl;
+    // Random number of hits between 1 and 5
+    int hits = rand() % 5 + 1;
+
+    std::cout << "Malenia uses Waterfowl Dance!" << std::endl;
+    std::cout << "It hits " << hits << " times!" << std::endl;
+
+    for (int i = 0; i < hits; i++)
+    {
+        player->takeDamage(60);
+
+        std::cout << "Hit " << i + 1 << "!" << std::endl;
+    }
 }
 
 double Malenia::getLifesteal()
